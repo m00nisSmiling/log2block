b7cb2e6aaafdedd0dedd1f8d17e00dfbc9432387
moonissmiling <122709554+m00nisSmiling@users.noreply.github.com>
2025-12-14 15:00:31 +0700

    Create uninstall.py

---
+++ uninstall.py
@@ -0,0 +1,20 @@
+import os
+import time
+
+usrname = input("| Enter your server's username :> ")
+websvr = input("| Enter your web server's name :> ")
+
+if usrname == "root":
+ home = "/root"
+else:
+ home = f"/home/{usrname}"
+
+unban = f"{home}/unban.py"
+moni = f"{home}/moni.py"
+service = f"/etc/systemd/system/moni.service"
+log = f"/var/log/{websvr}/moni.log"
+
+os.system(f"rm -rf {unban} {moni} {service} {log}")
+time.sleep(1)
+os.system("rm -rf ./uninstall.py")
+print("[!] Uninstalled moni service... ")
